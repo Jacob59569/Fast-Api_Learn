159b87bc3d5b30b7e285fd6f41b155309311e3cd
Jacob <110312077+Jacob59569@users.noreply.github.com>
2025-06-15 12:58:55 +0300

    add first main file

--- app/main.py
+++ app/main.py
@@ -1,19 +1,55 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 import uvicorn
 from typing import Optional
 from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class SHotel(BaseModel):
+    adress: str
+    name: str
+    stars: int
+
+
+class SHotelsSearchArgs:
+    def __init__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            stars: Optional[int] = Query(None, ge=1, le=5),
+            has_spa: Optional[bool] = None
+        ):
+            self.location = location
+            self.date_from = date_from
+            self.date_to = date_to
+            self.stars = stars
+            self.has_spa = has_spa
+
 @app.get('/hotels/')
 def get_hotels(
-        location: str,
-        date_from: date,
-        date_to: date,
-        stars: Optional[int] = Query(None, ge=1, le=5),
-        has_spa: Optional[bool] = None,
-):
-    return date_from, date_to
+        searchargs: SHotelsSearchArgs = Depends()
+) -> list[SHotel]:
+    hotels = [
+        {
+            'adress': 'Gagarina street',
+            'name': 'Super Hotel',
+            'stars': 5
+        }
+    ]
+
+    return hotels
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post('/booking')
+def add_booking(booking: SBooking):
+    pass
 
 
 
